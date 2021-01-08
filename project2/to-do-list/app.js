0e191a1ca35266e472d3be5e3384bbbd98659697
Jozef Wolf <jozefwolf@o2.pl>
2021-01-08 14:50:20 +0100

    format code

--- project2/to-do-list/app.js
+++ project2/to-do-list/app.js
@@ -10,45 +10,44 @@ const input = document.getElementById('input');
 //adding function to + button
 addBtn.addEventListener('click', function () {
 
-//creating elements 
-    const toDoTask = document.createElement('li');   
+    //creating elements 
+    const toDoTask = document.createElement('li');
     const doneBtn = document.createElement('button');
     const rmBtn = document.createElement('button');
 
-//text of task     
+    //text of task     
     toDoTask.innerText = input.value;
 
-//adding task and remove button and done button    
+    //adding task and remove button and done button    
     toDoContainer.append(toDoTask);
     toDoTask.appendChild(rmBtn);
     toDoTask.appendChild(doneBtn);
-    
 
-//task added with input 
+    //task added with input 
     input.value = '';
 
-//buttons style    
+    //buttons style    
     doneBtn.innerHTML = '<i class="fas fa-check"></i>';
     rmBtn.innerHTML = '<i class="far fa-trash-alt"></i>';
 
 
-//adding function to done button action
+    //adding function to done button action
     doneBtn.addEventListener('click', function () {
         toDoTask.style.textDecoration = 'line-through';
 
     })
-//adding function to remove button action 
+    //adding function to remove button action 
     rmBtn.addEventListener('click', function () {
         toDoContainer.removeChild(toDoTask);
     })
 })
 
-input.addEventListener("keyup", function(event) {
+input.addEventListener("keyup", function (event) {
     // Number 13 is the "Enter" key on the keyboard
     if (event.keyCode === 13) {
-      // Cancel the default action, if needed
-      event.preventDefault();
-      // Trigger the button element with a click
-      document.getElementById("add").click();
+        // Cancel the default action, if needed
+        event.preventDefault();
+        // Trigger the button element with a click
+        document.getElementById("add").click();
     }
-  });
+});
