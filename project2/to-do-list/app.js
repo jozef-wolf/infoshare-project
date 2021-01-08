10cb6ccef1ca61a5cdd08b5bde9e1fa0b0f9821d
Jozef Wolf <jozefwolf@o2.pl>
2021-01-08 13:33:53 +0100

    js - add and remove buttons added

--- project2/to-do-list/app.js
+++ project2/to-do-list/app.js
@@ -7,13 +7,36 @@ const input = document.getElementById('input');
 
 
 
-addBtn.addEventListener('click', function() {
+//adding function to + button
+addBtn.addEventListener('click', function () {
+
+//creating elements 
     const toDoTask = document.createElement('p');
-    // toDoTask.classList.add('') 
+    const rmBtn = document.createElement('button');
+    const doneBtn = document.createElement('button');
+
+//text of task     
     toDoTask.innerText = input.value;
+
+//adding task and remove button and done button    
     toDoContainer.append(toDoTask);
+    toDoTask.appendChild(rmBtn);
+    toDoTask.appendChild(doneBtn);
+
+//task added with input 
     input.value = '';
-    toDoTask.addEventListener('click', function(){
+
+//buttons style    
+    rmBtn.style.backgroundColor = 'red';
+    doneBtn.style.backgroundColor = 'green';
+
+//adding function to done button action
+    doneBtn.addEventListener('click', function () {
         toDoTask.style.textDecoration = 'line-through';
+        toDoTask.style.color = 'green';
+    })
+//adding function to remove button action 
+    rmBtn.addEventListener('click', function () {
+        toDoContainer.removeChild(toDoTask);
     })
 })
