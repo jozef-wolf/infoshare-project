ccdaa06435bb8d434745193ef46aff506a46800f
Jozef Wolf <jozefwolf@o2.pl>
2021-01-13 14:41:04 +0100

    index.html:6 correciton

--- project2/slide-show/main.js
+++ project2/slide-show/main.js
@@ -2,9 +2,20 @@
 //naming elements 
 
 const slideImage = document.querySelectorAll('.slide-image');
-const slidesContainer = document.querySelector('.slide-container');
+const slidesContainer = document.querySelector('.slides-container');
 const nextBtn = document.querySelector('.next-btn');
 const prevBtn = document.querySelector('.prev-btn');
 
 //slider 
 
+function init() {
+
+    slideImage.forEach((img, i) => {
+        img.style.left = i * 100 + '%';
+    });
+
+    slideImage[0].classList.add('active');
+}
+init();
+
+console.log(slideImage);
