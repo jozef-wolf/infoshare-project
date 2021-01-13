5611521be27be4520d144a26c219dd501d387ff3
Jozef Wolf <jozefwolf@o2.pl>
2021-01-13 13:52:20 +0100

    main.js - elements named

---
+++ project2/slide-show/main.js
@@ -0,0 +1,10 @@
+
+//naming elements 
+
+const slideImage = document.querySelectorAll('.slide-image');
+const slidesContainer = document.querySelector('.slide-container');
+const nextBtn = document.querySelector('.next-btn');
+const prevBtn = document.querySelector('.prev-btn');
+
+//slider 
+
