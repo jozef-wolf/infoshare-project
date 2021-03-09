8f4f972e5743c36c9cc22e137af381ade319556b
jozef-wolf <jozefwolf@o2.pl>
2021-03-09 12:43:54 +0100

    done toggle button

--- project2/to-do-list/app.js
+++ project2/to-do-list/app.js
@@ -1,66 +1,59 @@
-
-
-const addBtn = document.getElementById('add');
-const toDoContainer = document.getElementById('toDoContainer');
-const input = document.getElementById('input');
-
-
-
+const addBtn = document.getElementById("add");
+const toDoContainer = document.getElementById("toDoContainer");
+const input = document.getElementById("input");
 
 //adding function to + button
-addBtn.addEventListener('click', function () {
-    if (input.value.trim() === '') {
-        alert('Cannot add empty task');
-        return;
-    }
-    //creating elements 
-    const toDoTask = document.createElement('div');
-    const doneBtn = document.createElement('button');
-    const rmBtn = document.createElement('button');
+addBtn.addEventListener("click", function () {
+  if (input.value.trim() === "") {
+    alert("Cannot add empty task");
+    return;
+  }
+  //creating elements
+  const toDoTask = document.createElement("div");
+  const doneBtn = document.createElement("button");
+  const rmBtn = document.createElement("button");
 
-    //text of task     
-    toDoTask.innerText = input.value;
+  //text of task
+  toDoTask.innerText = input.value;
+  toDoTask.classList.add("task");
+  //adding task and remove button and done button
+  toDoContainer.append(toDoTask);
+  toDoTask.appendChild(rmBtn);
+  toDoTask.appendChild(doneBtn);
 
-    //adding task and remove button and done button    
-    toDoContainer.append(toDoTask);
-    toDoTask.appendChild(rmBtn);
-    toDoTask.appendChild(doneBtn);
+  //task added with input
+  input.value = "";
 
-    //task added with input 
-    input.value = '';
+  //buttons style
+  doneBtn.innerHTML = '<i class="fas fa-check"></i>';
+  rmBtn.innerHTML = '<i class="far fa-trash-alt"></i>';
 
-    //buttons style    
-    doneBtn.innerHTML = '<i class="fas fa-check"></i>';
-    rmBtn.innerHTML = '<i class="far fa-trash-alt"></i>';
+  //adding function to done button action
 
-
-    //adding function to done button action
-    doneBtn.addEventListener('click', function () {
-        toDoTask.style.textDecoration = 'line-through';
-
-    })
-    //adding function to remove button action 
-    rmBtn.addEventListener('click', function () {
-        toDoContainer.removeChild(toDoTask);
-    })
-})
+  function myFunction() {}
+  doneBtn.addEventListener("click", function () {
+    toDoTask.classList.toggle("task--done");
+  });
+  //adding function to remove button action
+  rmBtn.addEventListener("click", function () {
+    toDoContainer.removeChild(toDoTask);
+  });
+});
 
 //input value required
 
 function emptyValue() {
-    if (input.value.trim() === '') {
-        return;
-    }
+  if (input.value.trim() === "") {
+    return;
+  }
 }
 
-
-
 input.addEventListener("keyup", function (event) {
-    // Number 13 is the "Enter" key on the keyboard
-    if (event.keyCode === 13) {
-        // Cancel the default action, if needed
-        event.preventDefault();
-        // Trigger the button element with a click
-        document.getElementById("add").click();
-    }
+  // Number 13 is the "Enter" key on the keyboard
+  if (event.keyCode === 13) {
+    // Cancel the default action, if needed
+    event.preventDefault();
+    // Trigger the button element with a click
+    document.getElementById("add").click();
+  }
 });
