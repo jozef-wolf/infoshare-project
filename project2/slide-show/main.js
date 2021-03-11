52447462ca696c3547b8893a7f2d9e8b76a57775
jozef-wolf <jozefwolf@o2.pl>
2021-03-11 13:36:42 +0100

    wrapper styled

--- project2/slide-show/main.js
+++ project2/slide-show/main.js
@@ -1,4 +1,3 @@
-const slider = document.querySelector(".slider");
 const sliderContainer = document.querySelector(".slider__container");
 const previousBtn = document.querySelector(".previous");
 const nextBtn = document.querySelector(".next");
