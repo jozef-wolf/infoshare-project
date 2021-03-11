42f025c951b1631942b36cf6bb215201f35a9aaf
jozef-wolf <jozefwolf@o2.pl>
2021-03-11 12:38:12 +0100

    slider working next previous

--- project2/slide-show/main.js
+++ project2/slide-show/main.js
@@ -2,3 +2,40 @@ const slider = document.querySelector(".slider");
 const sliderContainer = document.querySelector(".slider__container");
 const previousBtn = document.querySelector(".previous");
 const nextBtn = document.querySelector(".next");
+const slideImage = document.querySelectorAll(".slider__container--image");
+
+let numberOfImages = slideImage.length;
+let width = slideImage[0].clientWidth;
+let activeSlide = 0;
+
+function slide() {
+  slideImage.forEach((img, i) => {
+    img.style.left = i * 100 + "%";
+  });
+
+  slideImage[0].classList.add("active");
+}
+slide();
+
+function nextSlide(slideNumber) {
+  sliderContainer.style.transform =
+    "translateX(-" + width * slideNumber + "px)";
+}
+
+nextBtn.addEventListener("click", () => {
+  if (activeSlide === 7) {
+    activeSlide = -1;
+    return;
+  }
+  activeSlide++;
+  nextSlide(activeSlide);
+});
+
+previousBtn.addEventListener("click", () => {
+  if (activeSlide === 0) {
+    activeSlide = 8;
+    return;
+  }
+  activeSlide--;
+  nextSlide(activeSlide);
+});
