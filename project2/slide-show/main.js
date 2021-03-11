e37b4ad49c6a174b00410236453ef5ea3ffc2e0c
jozef-wolf <jozefwolf@o2.pl>
2021-03-11 12:10:25 +0100

    js variables

---
+++ project2/slide-show/main.js
@@ -0,0 +1,4 @@
+const slider = document.querySelector(".slider");
+const sliderContainer = document.querySelector(".slider__container");
+const previousBtn = document.querySelector(".previous");
+const nextBtn = document.querySelector(".next");
